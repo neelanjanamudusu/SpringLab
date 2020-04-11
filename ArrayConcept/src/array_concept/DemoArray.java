be3d947be2f3a45764540969e4acb1449e28f283
Neelanjana <Neelanjana@LAPTOP-OBCOUTA5>
2020-04-11 09:53:31 +0530

    commit

---
+++ ArrayConcept/src/array_concept/DemoArray.java
@@ -0,0 +1,17 @@
+package array_concept;
+
+//import java.util.Scanner;
+
+public class DemoArray {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n[]=new int[5]; //array initialization
+int j;
+//Scanner s[]=new Scanner(System.in); 
+n[0]=10;
+n[2]=10;
+//int a[]={10,20,30,40,50};//array initialization
+for(int i=0;i<n.length;i++)
+{
+	System.out.println(n[i]);
+}}}
