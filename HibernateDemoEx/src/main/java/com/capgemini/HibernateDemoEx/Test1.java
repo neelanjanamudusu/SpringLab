68106a3e6d9edf11f7bd0cab314fb5aba556b985
Neelanjana <Neelanjana@LAPTOP-OBCOUTA5>
2020-04-11 10:04:39 +0530

    commit

---
+++ HibernateDemoEx/src/main/java/com/capgemini/HibernateDemoEx/Test1.java
@@ -0,0 +1,26 @@
+package com.capgemini.HibernateDemoEx;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+public class Test1 {
+public static void main(String[] args) {
+	Connection cfg=new Configuration();
+	cfg.configure("hibernate.cfg.xml");
+	SessionFactory factory=cfg.buildSessionFactory();
+	Session session=factory.openSession();
+	Transaction t=session.beginTransaction();
+	First f1=new First();
+	f1.setEid(1);
+	f1.setEname("neelu");
+	f1.setEsal(5000);
+	session.save(f1);
+	t.commit();
+	System.out.println("success");
+	session.close();
+	factory.close();
+}
+}
